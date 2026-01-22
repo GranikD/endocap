ab2709c2fda567f72fa1e80a4005ed792891ba34
Пиминов <piminov_a12@inbox.ru>
2026-01-22 17:58:49 +0700

    Update тренировка_нейронки.py

--- тренировка_нейронки.py
+++ тренировка_нейронки.py
@@ -1,40 +1,24 @@
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from monai.data import CacheDataset
-from monai.transforms import LoadImaged, EnsureChannelFirstd, ScaleIntensityd
+from tqdm import tqdm
 
-
-class CapsuleDataset(Dataset):
-    def __init__(self, image_paths, mask_paths):
-        self.image_paths = image_paths
-        self.mask_paths = mask_paths
-        self.transform = Compose([...])  # Ваши трансформации
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        image = cv2.imread(self.image_paths[idx])
-        mask = cv2.imread(self.mask_paths[idx], 0)
-        augmented = self.transform(image=image, mask=mask)
-        return augmented['image'], augmented['mask']
-
-
-# Пример обучения
-dataset = CacheDataset(data=[{'image': 'img.nii', 'label': 'mask.nii'}], transform=transforms)
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
 criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-for epoch in range(50):
-    for images, masks in dataloader:
-        images, masks = images.to(device), masks.to(device)
+epochs = 10
+
+for epoch in range(epochs):
+    model.train()
+    total_loss = 0
+
+    for images, labels in tqdm(train_loader):
+        images, labels = images.to(device), labels.to(device)
+
         optimizer.zero_grad()
         outputs = model(images)
-        loss = criterion(outputs, masks)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-    print(f'Epoch {epoch}, Loss: {loss.item()}')
 
-torch.save(model.state_dict(), 'unet_capsule.pth')
+        total_loss += loss.item()
+
+    print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader):.4f}")
